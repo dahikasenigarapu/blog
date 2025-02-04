c060101bdcbfbb7a09391a2e4a6bb648ec4adb08
dahikasenigarapu <dahikasenigarapu@gmail.com>
2025-02-04 16:11:57 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+// Theme toggle functionality
+const toggleButton = document.getElementById('theme-toggle');
+
+toggleButton.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
